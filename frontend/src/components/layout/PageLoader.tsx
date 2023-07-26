fe04e63352091999f95aa257c7057d773cc3e407
arikkfir <arik@kfirs.com>
2023-07-27 00:02:01 +0300

    chore(backend): add TODO comment

--- frontend/src/components/layout/PageLoader.tsx
+++ frontend/src/components/layout/PageLoader.tsx
@@ -1,6 +1,7 @@
 import {Box, CircularProgress} from "@mui/material";
 
 export function PageLoader() {
+    // TODO: improve page loader
     return (
         <Box sx={{display: 'flex'}}>
             <CircularProgress/>
