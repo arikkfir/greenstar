e4726be315c3921c726fa7185c8dba81ea9a5486
Arik Kfir <arik@kfirs.com>
2023-09-09 01:36:50 +0300

    feat(gha): add playwright tests & GHA workflow (#36)
    feat(ci): add end-to-end testing harness
    
    This change introduces end-to-end (e2e) testing to the project CI. The
    following changes and additions were made:
    
    1. Added the "e2e" module that contains a Playwright harness
    2. Added example tests (real tests to follow in separate PRs)
    3. PR deployment workflow runs the tests, and reports in PR comment
    4. Report comment provides link to test results with trace information

---
+++ e2e/tests/example.spec.ts
@@ -0,0 +1,21 @@
+import {expect, test} from '@playwright/test';
+
+// TODO: https://docs.descope.com/knowledgebase/testing/cypresstesting/
+
+const ENV_NAME = process.env.ENV_NAME;
+if (!ENV_NAME) {
+    throw new Error('ENV_NAME is not defined');
+}
+
+test('has title', async ({page}) => {
+    const url = `https://acme.${ENV_NAME}.greenstar.kfirs.com/`;
+    console.info(`Navigating to: ${url}`)
+
+    await page.goto(url, {
+        timeout: 1000 * 60,
+    });
+    await expect(page).toHaveTitle("GreenSTAR", {
+        timeout: 1000 * 60,
+    });
+});
+
