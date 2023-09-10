496cacddad081a8ecb3e5af5391764f607e13dd2
arikkfir <arik@kfirs.com>
2023-09-10 11:42:57 +0300

    fix(ci): Improve performance
    This change improves the CI workflows performance by making the
    following changes:
    
    1. Remove the Growthbook dependency patch, which makes the "jq" install
       step unnecessary; this allows removing the frontend Docker layer that
       invokes "apt-get" and installs "jq".
    2. Fixed Docker image caching to be image-specific, which ensures that
       caches from different images do not override each other.
    3. Split the end-to-end testing jobs into two jobs: test and report.
       1. The test job runs in a Playwright container that comes ready with
          all needed dependencies
       2. The report job just publishes the testing results back to the
          current PR or commit.
    
    In addition, the following changes have been made along the way:
    
    1. Remove Skaffold-related cruft in backend Dockerfile
    2. Remove the no-longer-needed frontend Dockerfile.debug (since we've
       switched to Telepresence)
    3. Catch & report errors from Growthbook feature loading
    4. Use pre-installed Google Cloud SDK which avoids downloading the whole
       600MB SDK

--- frontend/fix-growthbook-package-json.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -exuo pipefail
-
-jq '.exports.import={"types": "./dist/index.d.ts", "default": "./dist/esm/index.js"}' node_modules/@growthbook/growthbook-react/package.json > tmp.json
-mv tmp.json node_modules/@growthbook/growthbook-react/package.json
