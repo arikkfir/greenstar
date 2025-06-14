4ccedd01cd4fa0918f63b945460fed76f300e92e
arikkfir <arikkfir@gmail.com>
2025-06-14 19:53:25 +0300

    feat(scrapers): add new "scrapers" module with first scraper for Bank Yahav
    Add a new `scrapers` package intended to house the various accounts &
    transactions data from remote data sources.  Add the first scraper for Bank
    Yahav.

---
+++ scrapers/src/graphql/gql.ts
@@ -0,0 +1,52 @@
+/* eslint-disable */
+import * as types from './graphql';
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+
+/**
+ * Map of all GraphQL operations in the project.
+ *
+ * This map has several performance disadvantages:
+ * 1. It is not tree-shakeable, so it will include all operations in the project.
+ * 2. It is not minifiable, so the string of a GraphQL query will be multiple times inside the bundle.
+ * 3. It does not support dead code elimination, so it will add unused operations.
+ *
+ * Therefore it is highly recommended to use the babel or swc plugin for production.
+ * Learn more about it here: https://the-guild.dev/graphql/codegen/plugins/presets/preset-client#reducing-bundle-size
+ */
+type Documents = {
+    "\n    query GetTransactionsCount($tenantID: ID!) {\n        tenant(id: $tenantID) {\n            totalTransactions\n        }\n    }\n": typeof types.GetTransactionsCountDocument,
+    "\n    mutation CreateTransaction($tx: CreateTransaction!) {\n        createTransaction(tx: $tx) {\n            id\n        }\n    }\n": typeof types.CreateTransactionDocument,
+};
+const documents: Documents = {
+    "\n    query GetTransactionsCount($tenantID: ID!) {\n        tenant(id: $tenantID) {\n            totalTransactions\n        }\n    }\n": types.GetTransactionsCountDocument,
+    "\n    mutation CreateTransaction($tx: CreateTransaction!) {\n        createTransaction(tx: $tx) {\n            id\n        }\n    }\n": types.CreateTransactionDocument,
+};
+
+/**
+ * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ *
+ *
+ * @example
+ * ```ts
+ * const query = gql(`query GetUser($id: ID!) { user(id: $id) { name } }`);
+ * ```
+ *
+ * The query argument is unknown!
+ * Please regenerate the types.
+ */
+export function gql(source: string): unknown;
+
+/**
+ * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function gql(source: "\n    query GetTransactionsCount($tenantID: ID!) {\n        tenant(id: $tenantID) {\n            totalTransactions\n        }\n    }\n"): (typeof documents)["\n    query GetTransactionsCount($tenantID: ID!) {\n        tenant(id: $tenantID) {\n            totalTransactions\n        }\n    }\n"];
+/**
+ * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function gql(source: "\n    mutation CreateTransaction($tx: CreateTransaction!) {\n        createTransaction(tx: $tx) {\n            id\n        }\n    }\n"): (typeof documents)["\n    mutation CreateTransaction($tx: CreateTransaction!) {\n        createTransaction(tx: $tx) {\n            id\n        }\n    }\n"];
+
+export function gql(source: string) {
+  return (documents as any)[source] ?? {};
+}
+
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
