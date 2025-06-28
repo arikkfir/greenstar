2db3a6cbb99770bff446b43391e341b42625514e
arikkfir <arikkfir@gmail.com>
2025-06-28 16:14:05 +0300

    fix(migrations): add constraint to `display_name` in the tenants table
    Add a `CHECK` constraint to ensure `display_name` in the `tenants` table
    has a length greater than 0. This prevents invalid or empty data from being
    inserted into the column.

--- deploy/chart/files/migrations/V030__tenants.sql
+++ deploy/chart/files/migrations/V030__tenants.sql
@@ -3,6 +3,6 @@ CREATE TABLE tenants
     id           VARCHAR(10) PRIMARY KEY,
     created_at   TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
     updated_at   TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-    display_name TEXT
+    display_name TEXT CONSTRAINT display_name_length CHECK (CHAR_LENGTH(display_name) > 0)
 );
 GRANT SELECT, DELETE, INSERT, UPDATE ON TABLE tenants TO greenstar_server;
