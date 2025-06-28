a4a31b7ee99e799d8dcdaccabba4577720f0878e
arikkfir <arikkfir@gmail.com>
2025-06-28 16:29:19 +0300

    refactor(migrations): enhance `transactions` table constraints and structure
    Reorder columns in the `transactions` table for better clarity. Update
    constraint definitions, including naming `CHECK` and `FOREIGN KEY` constraints
    for improved readability. Simplify the `PRIMARY KEY` setup and rename unique
    constraint columns to ensure logical consistency.
    
    These changes improve schema maintainability and ensure data integrity.

--- deploy/chart/files/migrations/V050__transactions.sql
+++ deploy/chart/files/migrations/V050__transactions.sql
@@ -1,19 +1,13 @@
 CREATE TABLE transactions
 (
-    tenant_id         VARCHAR(10)       NOT NULL
-        CONSTRAINT fk_transactions_tenant_id
-            REFERENCES tenants
-            ON UPDATE CASCADE ON DELETE CASCADE,
-    id                TEXT       NOT NULL      DEFAULT gen_random_uuid(),
-    created_at        TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-    updated_at        TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    id                TEXT        NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at        TIMESTAMP WITH TIME ZONE         DEFAULT NOW(),
+    updated_at        TIMESTAMP WITH TIME ZONE         DEFAULT NOW(),
     date              TIMESTAMP WITH TIME ZONE,
-    sequence          SMALLINT   NOT NULL      DEFAULT 0 CHECK ( sequence >= 0 ),
-    reference_id      TEXT       NOT NULL,
-    amount            NUMERIC    NOT NULL CHECK (amount > 0),
-    currency          VARCHAR(5) NOT NULL CHECK (CHAR_LENGTH(currency) = 3)
     sequence          SMALLINT    NOT NULL             DEFAULT 0
         CONSTRAINT sequence_gt_0 CHECK ( sequence >= 0 ),
+    reference_id      TEXT        NOT NULL
+        CONSTRAINT reference_id_not_empty CHECK (CHAR_LENGTH(reference_id) > 0),
     amount            NUMERIC     NOT NULL
         CONSTRAINT amount_gt_0 CHECK (amount > 0),
     currency          VARCHAR(5)  NOT NULL
@@ -21,17 +15,22 @@ CREATE TABLE transactions
         CONSTRAINT fk_transactions_currency
             REFERENCES currencies
             ON UPDATE CASCADE ON DELETE RESTRICT,
-    description       TEXT CHECK (CHAR_LENGTH(description) > 0),
-    source_account_id TEXT       NOT NULL,
-    target_account_id TEXT       NOT NULL,
-    PRIMARY KEY (tenant_id, id),
     description       TEXT
         CONSTRAINT description_not_empty CHECK (CHAR_LENGTH(description) > 0),
+    source_account_id TEXT        NOT NULL,
+    target_account_id TEXT        NOT NULL,
+    tenant_id         VARCHAR(10) NOT NULL
+        CONSTRAINT fk_transactions_tenant_id
+            REFERENCES tenants (id)
+            ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT uq_transactions UNIQUE (
-                                       tenant_id, date, reference_id, amount, currency,
-                                       description, source_account_id, target_account_id
+                                       date, reference_id, amount, currency, description,
+                                       source_account_id, target_account_id, tenant_id
         ),
-    FOREIGN KEY (tenant_id, source_account_id) REFERENCES accounts (tenant_id, id) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (tenant_id, target_account_id) REFERENCES accounts (tenant_id, id) ON UPDATE CASCADE ON DELETE CASCADE
+    CONSTRAINT fk_transactions_source_account_id FOREIGN KEY (tenant_id, source_account_id) REFERENCES accounts (tenant_id, id)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_transactions_target_account_id FOREIGN KEY (tenant_id, target_account_id) REFERENCES accounts
+        (tenant_id, id)
+        ON UPDATE CASCADE ON DELETE CASCADE
 );
 GRANT SELECT, DELETE, INSERT, UPDATE ON TABLE transactions TO greenstar_server;
