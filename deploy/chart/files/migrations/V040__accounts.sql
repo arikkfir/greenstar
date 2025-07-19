06233a328a53193acfeb7825c5c3babf415d7d45
Arik Kfir <arik@kfirs.com>
2025-07-19 17:25:57 +0300

    chore(db): name column validation constraints with common prefix (#445)
    Ensure all column validation constraints start with the `val_` prefix.
    
    ---------
    
    Signed-off-by: Arik Kfir <arik@kfirs.com>

--- deploy/chart/files/migrations/V040__accounts.sql
+++ deploy/chart/files/migrations/V040__accounts.sql
@@ -7,8 +7,10 @@ CREATE TABLE accounts
     id           TEXT NOT NULL            DEFAULT gen_random_uuid(),
     created_at   TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
     updated_at   TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-    display_name TEXT NOT NULL CHECK (CHAR_LENGTH(display_name) > 0),
-    icon         TEXT NOT NULL CHECK (CHAR_LENGTH(icon) > 0),
+    display_name TEXT NOT NULL
+        CONSTRAINT val_display_name_length CHECK (CHAR_LENGTH(display_name) > 0),
+    icon         TEXT NOT NULL
+        CONSTRAINT val_icon_length CHECK (CHAR_LENGTH(icon) > 0),
     type         TEXT,
     parent_id    TEXT,
     PRIMARY KEY (tenant_id, id),
