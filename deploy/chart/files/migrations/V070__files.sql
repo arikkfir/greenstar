51c009f3f7bddb300079fc90de2d55d56a225096
arikkfir <arikkfir@gmail.com>
2025-07-21 14:45:09 +0300

    wip

--- deploy/chart/files/migrations/V070__files.sql
+++ deploy/chart/files/migrations/V070__files.sql
@@ -1,10 +1,10 @@
 CREATE TABLE files
 (
     id           TEXT        NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
-    name         TEXT        NOT NULL,
+    name         TEXT        NOT NULL CONSTRAINT val_name_length CHECK (CHAR_LENGTH(name) > 0),
     data_oid     oid         NOT NULL,
     size         INTEGER     NOT NULL,
-    content_type TEXT        NOT NULL,
+    content_type TEXT        NOT NULL CONSTRAINT val_content_type_length CHECK (CHAR_LENGTH(content_type) > 0),
     tenant_id    VARCHAR(10) NOT NULL
         CONSTRAINT fk_files_tenant_id
             REFERENCES tenants (id)
